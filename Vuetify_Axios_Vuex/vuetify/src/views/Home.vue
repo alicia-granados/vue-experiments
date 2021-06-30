4f107baa5a8c773caf4ce77c3c3a89666ddee6f5
alicia-granados <aliciagranados35@gmail.com>
2021-06-29 22:36:01 -0500

    feat: :sparkles: Vuetify (Grid, Colores, Tipografía, Espaciados)

---
+++ Vuetify_Axios_Vuex/vuetify/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-main>
+        <v-layout wrap>
+            <v-flex class ="primary" xs12>
+                <h1>12</h1>
+            </v-flex>
+            <v-flex class ="secondary" xs6>
+                <h1>6</h1>
+            </v-flex>
+            <v-flex class ="error" xs6>
+                <h1>6</h1>
+            </v-flex>
+        </v-layout>
+
+        <v-layout class="mt-5 secondary" justify-center align-center style="height:300px">
+            <v-flex class ="info"  xs3>
+                <h1>centrado</h1>
+            </v-flex>
+              <v-flex class ="pink lighten-1 white--text"  xs3>
+                <h1>centrado</h1>
+            </v-flex>
+        </v-layout>
+
+         <v-layout>
+            <v-flex>
+                <h1 class ="display-1 text-uppercase" >Lorem, ipsum dolor.</h1>
+                <h1 class ="display-2 primary pa-5 mt-5" >Lorem, ipsum dolor.</h1>
+                <h1 class ="display-3 text-center" >Lorem, ipsum dolor.</h1>
+                <h1 class ="display-4" >Lorem, ipsum dolor.</h1>
+            </v-flex>
+        </v-layout>
+    </v-main>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
